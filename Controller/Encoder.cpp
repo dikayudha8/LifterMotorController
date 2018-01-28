8c0b06a6d755875769b9fffe71e877d1f2db443d
dikayudha8 <andika@hopetechnik.com>
2018-01-28 23:38:27 +0800

    adding another prototype class for PID

--- Controller/Encoder.cpp
+++ Controller/Encoder.cpp
@@ -10,20 +10,90 @@ Encoder::Encoder() {
   digitalWrite(CLK, LOW);
   SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
   delay(5);
+
+  ReadCalibratedOffset();
+  ReadInitialOffset();
 }
 
 Encoder::~Encoder() {
 
 }
 
+void Encoder::WriteEEPROM(uint8_t address, uint16_t valueToWrite) {
+  IntToBytes intToBytes;
+  uint8_t checkSum;
+  intToBytes.integer = valueToWrite;
+  for (uint8_t i = 0; i < 3; ++i) {
+    if (i < 2) {
+      EEPROM.write(i + address, intToBytes.bytes[i]);
+      checkSum += intToBytes.bytes[i];
+    } else {
+      checkSum = ~checkSum;
+      EEPROM.write(i + address, checkSum);
+    }
+  }
+}
+
+uint16_t Encoder::ReadEEPROM(uint8_t address) {
+  IntToBytes intToBytes;
+  uint8_t readFromEEPROM[3];
+  uint8_t checkSum, validateCheckSum;
+  for (uint8_t i = 0; i < 3; ++i) {
+    readFromEEPROM[i] = EEPROM.read(i + address);
+    if (i < 2) {
+      intToBytes.bytes[i] = readFromEEPROM[i];
+      validateCheckSum += readFromEEPROM[i];
+    }
+  }
+  validateCheckSum = ~validateCheckSum;
+  checkSum = readFromEEPROM[2];
+  if (checkSum == validateCheckSum) {
+    uint16_t output = intToBytes.integer;
+    return output;
+  } else {
+    return 0;
+  }
+}
+uint8_t Encoder::ResetCalibration() {
+  WriteEEPROM(CALIBRATION_OFFSET_ADDR, 0);
+  rackHeightOffset = 0;
+  return 1;
+}
+
+uint8_t Encoder::Calibrate() {
+  WriteEEPROM(CALIBRATION_OFFSET_ADDR, rackHeight);
+  rackHeightOffset = rackHeight;
+  return 1;
+}
+
+uint8_t Encoder::InitialOffsetCalibration() {
+  WriteEEPROM(INITIAL_OFFSET_ADDR, rackHeightRaw);
+  initialOffset = rackHeightRaw;
+  return 1;
+}
+
+uint8_t Encoder::ReadCalibratedOffset() {
+  rackHeightOffset = ReadEEPROM(CALIBRATION_OFFSET_ADDR);
+  return rackHeightOffset;
+}
+uint8_t Encoder::ReadInitialOffset() {
+  initialOffset = ReadEEPROM(INITIAL_OFFSET_ADDR);
+  return initialOffset;
+}
+
 void Encoder::ReadEncoder() {
+  RAWMAXHEIGHT = RAWMAXHEIGHTDIFF + initialOffset;
   digitalWrite(SS, LOW);
   uint8_t result1 = SPI.transfer(0xFF);
   uint8_t result2 = SPI.transfer(0xFF);
   unsigned int resultRaw = (result1 << 8 | result2);
   unsigned int result = resultRaw >> 4;
-
-  rackHeightFiltered = ((HIGHPASS * rackHeightFiltered) + (LOWPASS * (float)result));
-  rackHeightRaw = (unsigned int) rackHeightFiltered;
+  digitalWrite(SS, HIGH);
+  
+  rackHeightRaw = result;
+  rackHeightFiltered = ((HIGHPASS * rackHeightFiltered) + (LOWPASS * (float)rackHeightRaw));
+  rackHeight = ((unsigned int) rackHeightFiltered) - initialOffset;
+  rackHeightCalibrated = rackHeightRaw - rackHeightOffset;
+  calibratedMaxHeight = (RAWMAXHEIGHT - (rackHeightOffset + initialOffset));
 }
 
--- Controller/Encoder.h
+++ Controller/Encoder.h
@@ -1,11 +1,15 @@
 #include <arduino.h>
 #include <SPI.h>
 #include <EEPROM.h>
+#include "SerialComm.h"
 
 #define RAWMAXHEIGHTDIFF 3067
 #define SS 10
 #define MISO 12
 #define CLK 13
+
+#define INITIAL_OFFSET_ADDR 32
+#define CALIBRATION_OFFSET_ADDR 16
 
 #define LOWPASS 0.1f
 #define HIGHPASS (1.0f - LOWPASS)
@@ -18,10 +22,19 @@ class Encoder {
     void ReadEncoder();
     uint8_t Calibrate();
     uint8_t ResetCalibration();    
-
+    uint8_t ReadCalibratedOffset();
+    uint8_t ReadInitialOffset();
+    uint8_t InitialOffsetCalibration();
   private:
+    uint16_t rackHeight;
     uint16_t rackHeightRaw;
     float rackHeightFiltered;
     uint16_t initialOffset;
+    uint16_t RAWMAXHEIGHT;
+    uint16_t rackHeightOffset;
+    uint16_t rackHeightCalibrated;
+    uint16_t calibratedMaxHeight;
+    uint16_t ReadEEPROM(uint8_t address);
+    void WriteEEPROM(uint8_t address, uint16_t valueToWrite);
 };
 
